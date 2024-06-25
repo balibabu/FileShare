bba58ba70904179032336f8e2eda9259f1ccafc7
balibabu <balibabuchauhan1@gmail.com>
2024-06-25 21:59:04 +0545

    added terminal qrcode

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask,render_template,send_file,request,redirect,jsonify
-import os,io
+from flask import Flask,render_template,send_file,request,jsonify
+import os
 import socket
 import qrcode
-import qrcode.image.svg
-import webbrowser
 
 app = Flask(__name__)
-DIRECTORY = "C:\\Users\\Lappy\\Desktop"
 PORT = 8000
+DIRECTORY=''
+
+
+if os.path.exists('path.txt'):
+    file=open('path.txt')
+    DIRECTORY=file.read()
+else:
+    upath=input('enter a path or help: ')
+    if upath=='help':
+        input('1. Give empty value for desktop location \n2. Create a path.txt file and paste a valid path in it\npress enter to exit')
+        exit()
+    elif upath=='':
+        DIRECTORY = os.path.join(os.path.expanduser("~"), "Desktop")
+    else:
+        DIRECTORY=upath
+        
 ipv4_address = socket.gethostbyname(socket.gethostname())
 url = f'http://{ipv4_address}:{PORT}'
 
@@ -32,15 +45,16 @@ def upload_file():
     file.save(os.path.join(DIRECTORY, file.filename))
     return jsonify({'message': 'File uploaded successfully', 'filename': file.filename})
 
-@app.route("/qrcode")
-def show_QR_CODE():
-    factory = qrcode.image.svg.SvgPathImage
-    img = qrcode.make(url, image_factory=factory)
-    output = io.BytesIO()
-    img.save(output)
-    svg_data = output.getvalue().decode()
-    return svg_data
+def show_qr_terminal(value):
+    qr = qrcode.QRCode(1,qrcode.constants.ERROR_CORRECT_L,  10,  4)
+    qr.add_data(value)
+    qr.make(fit=True)
+    qr_matrix = qr.get_matrix()
+    for row in qr_matrix:
+        for col in row:
+            print('\u2588'*2,end='') if col else print('  ',end='')
+        print()
 
 if __name__ == '__main__':
-    webbrowser.open(url+'/qrcode')
+    show_qr_terminal(url)
     app.run(host='0.0.0.0', port=PORT)
